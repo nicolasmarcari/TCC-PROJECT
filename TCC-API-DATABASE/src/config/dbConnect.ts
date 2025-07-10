b970fcec8a58f3dfddc062a99956a39c9eea3028
nicolasmarcari <nicolas.juner.marcari@gmail.com>
2025-07-10 20:39:32 -0300

    refact: remove connection

--- TCC-API-DATABASE/src/config/dbConnect.ts
+++ TCC-API-DATABASE/src/config/dbConnect.ts
@@ -1,15 +1,5 @@
 import mongoose from 'mongoose';
 
-export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI!);
-        console.log('MongoDB connected!');
-    } catch (err) {
-        console.error(`Error to connect on MongoDB. ${err}`);
-        process.exit(1);
-    }
-};
-
 class MongoDB {
     private readonly mongoURI: string;
 
