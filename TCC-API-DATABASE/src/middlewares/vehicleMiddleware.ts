b1250368e0bcc4818df91da792b71f6c62f7f18b
nicolasmarcari <nicolas.juner.marcari@gmail.com>
2025-07-10 20:49:18 -0300

    fix: fixing middleware validator

--- TCC-API-DATABASE/src/middlewares/vehicleMiddleware.ts
+++ TCC-API-DATABASE/src/middlewares/vehicleMiddleware.ts
@@ -1,17 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
-import { ZodSchema } from 'zod';
+import { ZodObject, ZodRawShape } from 'zod';
 
-export const validateVehicle = (schema: ZodSchema) => (
+export const validateVehicle = (schema: ZodObject<ZodRawShape>) => (
     req: Request,
     res: Response,
     next: NextFunction
 ): any => {
-    const result = schema.safeParse(req.body);
+    const result = schema.strict().safeParse(req.body);
 
     if (!result.success) {
         return res.status(400).json({
             message: 'Validation Error',
-            errors: result.error
+            errors: result.error.format()
         })
     }
 
