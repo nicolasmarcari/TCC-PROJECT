9cffd5d1360bcaa35b744e5cdea13d128a29ad16
Raphael Orati <misterbroderkill@gmail.com>
2025-08-15 19:23:39 -0300

    feat: Inclusão do método GET e inclusão do Método de exception GlobalException.

--- TCC-API-BACKEND/carbon/src/main/java/com/edu/tcc/carbon/carbon/controller/CarbonController.java
+++ TCC-API-BACKEND/carbon/src/main/java/com/edu/tcc/carbon/carbon/controller/CarbonController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,6 +19,7 @@ import com.edu.tcc.carbon.carbon.dto.CalculationResponseAllDataDTO;
 import com.edu.tcc.carbon.carbon.dto.CalculationResponseDTO;
 import com.edu.tcc.carbon.carbon.dto.dtoUser.CalculationRequestUserDTO;
 import com.edu.tcc.carbon.carbon.service.CarbonService;
+
 
 @RestController
 @RequestMapping("/api")
@@ -41,5 +45,32 @@ public class CarbonController {
         restTemplate.postForEntity(url,entityPost,String.class);
 
         return ResponseEntity.ok().body(carbonService.getCarbon(requestDTO));
+    }
+    @GetMapping("/getVehicle/{uuid}")
+    public @ResponseBody ResponseEntity<String> getMethodName(@RequestParam String uuid) {
+        headers.set("Authorization", authorizationToken);
+        
+        HttpEntity<String> entityGet = new HttpEntity<>(headers);
+        
+        String url = "http://localhost:3000/getVehicle/" + uuid;
+        
+        ResponseEntity<String> response = restTemplate.exchange(
+            url,
+            HttpMethod.GET,
+            entityGet,
+            String.class
+        );
+
+        if (response.getStatusCode().is4xxClientError()) {
+            return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+        }else if(response.getStatusCode().is2xxSuccessful()) {
+            return ResponseEntity.ok().body(response.getBody());
+        }else if(response.getStatusCode().is5xxServerError()) {
+            return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+        }else {
+            return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+        }
+    
+    
     }
 }
